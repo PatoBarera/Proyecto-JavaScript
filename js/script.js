dba68005c16dec3593810ec425c36e54cc40d289
Pato Barera <patriciobarera@gmail.com>
2022-10-04 12:46:18 -0300

    primer entrega

---
+++ js/script.js
@@ -0,0 +1,85 @@
+
+
+
+
+/* let miNombreCompleto = 'Patricio Barera';
+
+let fraseFavorita = 'Aguante La Renga';
+
+let palabraFavorita = 'Tortilla';
+
+let peso = 75;
+
+let altura = 1.77;
+
+let apellido = 'Barera';
+
+const TATUAJE = 'mama te amo';
+
+let numeroA = 4;
+let numeroB = 9;    
+
+let suma = numeroA + numeroB;
+let resta = numeroB - numeroA;
+
+let palabra1 = 'casa'; 
+
+ let nombreUsuario = prompt ('Ingresa tu nombre');
+let edadActual = parseInt (prompt('Ingresa tu edad actual')); */
+
+ /*let nombreUsuario = prompt ('Ingresa tu nombre');
+alert ('Bienvenido a mi sitio. Ya nunca podras salir' + nombreUsuario); 
+
+console.log ('el valor de la variable resta es'+ resta) 
+*/
+
+/* let condicion = true; 
+let otraCondicion = false;
+let edad = 17
+
+if (condicion) {
+console.log ('la condicion es verdadera')
+
+}
+
+/* if (edad==37){
+console.log ('la edad es 37')
+}
+*/
+
+/* if (edad>18){
+alert ('podes salir de jerga')
+} else {
+alert ('no pasarás')
+} */
+
+/* const edadObligatoria = 18 ;
+let edadOptativa = 16;
+
+let edadUser= parseInt(prompt ('ingresa tu edad')) ;
+
+if(edadUser>=edadObligatoria){
+    alert ('podes votar si queres');
+} else if (edadUser>=edadOptativa){
+    alert ('debes votar');  
+} else {
+    alert ('no podes votar');
+}
+*/
+
+/*
+let peso = prompt("Ingrese su peso en Kg");
+let altura = prompt ("Ingrese su altura en metros (Ej: 1.75)");
+let indice = peso / (altura **2 );
+alert("Tu IMC es " + indice)
+if (indice <= 18.4) {
+    alert("Estas bajo/a de peso");
+}
+if (indice >= 18.5 && indice <= 24.9) {
+    alert("Estas en el peso adecuado amigo/a")
+}
+if (indice >= 25.0 ) {
+    alert("Estas un poco pasado en tu peso amigo/a")
+
+} 
+*/
